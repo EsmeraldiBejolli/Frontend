4c4aa5959c08b7044658b5cbd350afe83f560653
EsmeraldiBejolli <esmeraldi.bejolli@gmail.com>
2023-02-17 00:41:26 +0100

    fix the inline css and javascript

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ const path = require('path');
 
 module.exports = {
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   devServer: {
     static: {
       directory: path.join(__dirname, 'public'),
